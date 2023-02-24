e2d5862de1c9d8d87d0f9f4667823e8fb5bdb413
usama zafar <usamazafar543@gmail.com>
2023-02-25 02:50:11 +0400

    deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
   import axios from 'axios';
 
-  const API = axios.create({ baseURL: 'http://localhost:5000' });
+  const API = axios.create({ baseURL: 'memories-api-7ejb.onrender.com' });
 
   API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
